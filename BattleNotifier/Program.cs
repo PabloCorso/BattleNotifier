1fb63ae647883660172cf0e05df508f4b2c24dc0
Pablo Corso <pabcorso@gmail.com>
2015-05-30 19:25:52 -0300

    HtmlAgilityPack embeded in .exe

--- BattleNotifier/Program.cs
+++ BattleNotifier/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Windows.Forms;
+using System.Reflection;
 using BattleNotifier.View;
 
 namespace BattleNotifier
@@ -14,6 +15,20 @@ namespace BattleNotifier
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
+            AppDomain.CurrentDomain.AssemblyResolve += (Object sender, ResolveEventArgs args) =>
+            {
+                String thisExe = System.Reflection.Assembly.GetExecutingAssembly().GetName().Name;
+                System.Reflection.AssemblyName embeddedAssembly = new System.Reflection.AssemblyName(args.Name);
+                String resourceName = thisExe + "." + embeddedAssembly.Name + ".dll";
+
+                using (var stream = System.Reflection.Assembly.GetExecutingAssembly().GetManifestResourceStream(resourceName))
+                {
+                    Byte[] assemblyData = new Byte[stream.Length];
+                    stream.Read(assemblyData, 0, assemblyData.Length);
+                    return System.Reflection.Assembly.Load(assemblyData);
+                }
+            };
+
             Main main = new Main();
             Application.Run(main);
             main.DisposeBattleNotification();
