d4785fc4096deb0d754cb28a3deca9a2fa001764
Pablo Corso <pabcorso@gmail.com>
2015-08-06 01:05:45 -0300

    Refactoring, comments.

--- BattleNotifier/View/MiddleClass.cs
+++ BattleNotifier/View/MiddleClass.cs
@@ -2,6 +2,11 @@
 
 namespace BattleNotifier.View
 {
+    /// <summary>
+    /// This class implements the abstract BaseNotification to be later inherited 
+    /// by concrete notification forms, this is the only way to make visual studio 
+    /// designer work with concrete forms that must inherit from the abstract class.
+    /// </summary>
     public class MiddleClass : BaseNotification
     {
         public MiddleClass() { }
