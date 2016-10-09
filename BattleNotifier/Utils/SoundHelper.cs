38873980f5d864ae95aab8ffdc418d73e89ed04d
PabloCorso <pabcorso@gmail.com>
2016-10-08 21:15:23 -0300

    Use SoundPlayer for Wav files in case user doesnt have Windows Media Player 10 or higher versions installed.

--- BattleNotifier/Utils/SoundHelper.cs
+++ BattleNotifier/Utils/SoundHelper.cs
@@ -9,23 +9,30 @@ namespace BattleNotifier.Utils
     public class SoundHelper
     {
         private MediaPlayer mediaPlayer;
+        private SoundPlayer soundPlayer;
+
+        public void PlayDefaultSound(int defaultSound)
+        {
+            soundPlayer = new SoundPlayer(IndexToDefaultSound(defaultSound));
+            soundPlayer.Play();
+        }
 
         public void PlaySound(string path, int defaultSound = 0)
         {
             try
             {
-                mediaPlayer = new MediaPlayer();
-                mediaPlayer.MediaFailed += (o, args) =>
+                if (IsWavSoundPath(path))
                 {
-                    Logger.Log(200, new Exception(args.ErrorException.Message));
-                };
-                mediaPlayer.Open(new Uri(path, UriKind.Absolute));
-
-                mediaPlayer.Play();
+                    PlayWavSound(path);
+                }
+                else
+                {
+                    PlayMediaPlayerSound(path);
+                }
             }
             catch (Exception ex)
             {
-                Logger.Log(200, ex);
+                Logger.Log(201, ex);
                 PlayDefaultSound(defaultSound);
             }
         }
@@ -34,6 +41,8 @@ namespace BattleNotifier.Utils
         {
             try
             {
+                if (soundPlayer != null)
+                    soundPlayer.Stop();
                 if (mediaPlayer != null)
                     mediaPlayer.Stop();
             }
@@ -43,9 +52,25 @@ namespace BattleNotifier.Utils
             }
         }
 
-        public void PlayDefaultSound(int defaultSound)
+        private bool IsWavSoundPath(string path)
         {
-            var soundPlayer = new SoundPlayer(IndexToDefaultSound(defaultSound));
+            return path.EndsWith(".wav");
+        }
+
+        private void PlayMediaPlayerSound(string path)
+        {
+            mediaPlayer = new MediaPlayer();
+            mediaPlayer.MediaFailed += (o, args) =>
+            {
+                Logger.Log(201, new Exception(args.ErrorException.Message));
+            };
+            mediaPlayer.Open(new Uri(path, UriKind.Absolute));
+            mediaPlayer.Play();
+        }
+
+        private void PlayWavSound(string path)
+        {
+            soundPlayer = new SoundPlayer(path);
             soundPlayer.Play();
         }
 
